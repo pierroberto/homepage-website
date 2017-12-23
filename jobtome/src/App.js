492707f520d24593bb448cf235147a77fd6b817a
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-12-23 17:03:12 +0100

    Chore: add basic structure search fiels

--- jobtome/src/App.js
+++ jobtome/src/App.js
@@ -10,9 +10,21 @@ class App extends Component {
     return (
       <div className="app">
         <Navbar></Navbar>
+        <section>
+          <img className="app__home" src={require("./assets/home.jpg")} />
+          <div className="app__wrapper">
+            <div className="app__input">
+              <input className ="app__inputBox" type="text" placeholder="city..."/>
+              <label className ="app__label">Title, Skill, Sector, Company name</label>
+            </div>
+            <div className="app__input">
+              <input className ="app__inputBox" type="text" placeholder="title..."/>
+              <label className ="app__label">City</label>
+              <button className ="app__button">Vai</button>
+            </div>
+          </div>
+        </section>
         <div className="app__container">
-          
-          <Search />
         </div>
       </div>
     );
--- jobtome/src/components/Search/Search.js
+++ jobtome/src/components/Search/Search.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 class Search extends Component {
   render() {
     return (
-      <div className="Search">
+      <div className="search">
         searchbar
       </div>
     );
